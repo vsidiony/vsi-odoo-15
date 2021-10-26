68f8410e05c41c048148657ea1c4c61a0c13c6de
vsidiony <diony.castro@vettersystems.com>
2021-10-26 11:58:16 +0800

    Update version

--- dsc_customsales/__manifest__.py
+++ dsc_customsales/__manifest__.py
@@ -16,11 +16,11 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '0.2',
     'sequence': -102,
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale','mail','account','website'],
+    'depends': ['base','sale'],
 
     # always loaded
     'data': [        
